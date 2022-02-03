b57e7a1a28e83271a34d7b036a9d4b00c3a17d89
sunilpowar <sunilpowarsp8468@gmail.com>
2022-02-04 02:44:59 +0530

    Adding main

---
+++ LinkedList/LinkedListOne.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class LinkedListOne
+    {
+        internal Node head; //new
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into the linked list", node.data);
+
+        }
+        internal void AddFirst(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into the Append linked list", node.data);
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("\nLinked list is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next; //temp=null
+            }
+            Console.WriteLine("\n");
+        }
+        internal Node InsertAtParticularPosition(int position, int data)
+        {
+            if (position < 1)
+                Console.WriteLine("Invalid position");
+            if (position == 1)
+            {
+                var newNode = new Node(data);
+                newNode.next = this.head;
+                head = newNode;
+            }
+            else
+            {
+                Node temp = this.head;
+                while (position-- != 0) //
+                {
+
+                    if (position == 1)
+                    {
+                        Node node = new Node(data);
+                        node.next = this.head.next;
+                        head.next = node;
+                        break;
+                    }
+                    temp = temp.next;//1000
+                }
+                if (position != 1)
+                    Console.WriteLine("Position out of range");
+            }
+            return head;
+        }
+        internal Node RemoveFirstNode()
+        {
+            if (this.head == null)
+                return null;
+            this.head = this.head.next;
+            return this.head;
+        }
+        internal Node RemovaLastNode()
+        {
+            if (head == null)
+                return null;
+            if (head.next == null)
+                return null;
+            Node newNode = head;
+            while (newNode.next.next != null)
+            {
+                newNode = newNode.next;
+            }
+            newNode.next = null;
+            return head;
+        }
+        internal Node Search(int value)
+        {
+            while (this.head != null)
+            {
+                if (this.head.data == value)
+                {
+                    return this.head;
+                }
+                this.head = this.head.next;
+            }
+            return null;
+        }
+        internal void RemoveNodeAtParticularPosition(int position)
+        {
+
+            // If linked list is empty
+            if (head == null)
+                return;
+
+            // Store head node
+            Node temp = head;
+
+            // If head needs to be removed
+            if (position == 0)
+            {
+
+                // Change head
+                head = temp.next;
+                return;
+            }
+
+            // Find previous node of the node to be deleted
+            for (int i = 0; temp != null && i < position - 1; i++)
+                temp = temp.next;
+
+            // If position is more than number of nodes
+            if (temp == null || temp.next == null)
+                return;
+
+            // Node temp->next is the node to be deleted
+            // Store pointer to the next of node to be deleted
+            Node next = temp.next.next;
+
+            // Unlink the deleted node from list
+            temp.next = next;
+        }
+        internal void sortList()
+        {
+            //Node current will point to head  
+            Node current = head, index = null;
+            int temp;
+
+            if (head == null)
+            {
+                return;
+            }
+            else
+            {
+                while (current != null)
+                {
+                    //Node index will point to node next to current  
+                    index = current.next;
+
+                    while (index != null)
+                    {
+                        //If current node's data is greater than index's node data, swap the data between them  
+                        if (current.data > index.data)
+                        {
+                            temp = current.data;
+                            current.data = index.data;
+                            index.data = temp;
+                        }
+                        index = index.next;
+                    }
+                    current = current.next;
+                }
+            }
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List");
+            LinkedListOne list2 = new LinkedListOne();
+            //Appends the Nodes
+            list2.AddFirst(56);
+            list2.AddFirst(30);
+            list2.AddFirst(70);
+
+            //Remove first node 50 from the linked list
+            list2.RemoveFirstNode();
+            list2.Display();
+            //Removed Last Node 70 from the linked list
+            Console.WriteLine("Removed the last node from the linked list");
+            LinkedListOne list5 = new LinkedListOne();
+            list5.Add(56);
+            list5.Add(30);
+            list5.Add(70);
+            list5.RemovaLastNode();
+            list5.Display();
+            //Search Node 30 from the given liked list
+            Console.WriteLine("Searching the Nodes");
+            LinkedListOne list6 = new LinkedListOne();
+            list6.Add(40);
+            list6.Add(30);
+            list6.Add(70);
+            //remove Node 40
+
+            list6.RemoveNodeAtParticularPosition(1);
+            list6.Display();
+            Console.ReadKey();
+            //Create a sorted linked list in ascending order
+            Console.WriteLine("create a sorted linked list in acsending order");
+            LinkedListOne list7 = new LinkedListOne();
+            list7.Add(56);
+            list7.Add(30);
+            list7.Add(40);
+            list7.Add(70);
+            Console.WriteLine("Before Sorting ");
+            list7.Display();
+            Console.WriteLine("After Sorting");
+            list7.sortList();
+            list7.Display();
+            Console.ReadLine();
+        }
+
+    }
+}
