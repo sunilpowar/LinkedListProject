056048ab0fabf3ccad266f4198d8c54a46eea2ef
sunilpowar <sunilpowarsp8468@gmail.com>
2022-02-04 03:11:21 +0530

    Adding UC10

--- LinkedList/LinkedListOne.cs
+++ LinkedList/LinkedListOne.cs
@@ -8,14 +8,12 @@ namespace LinkedList
 {
     internal class LinkedListOne
     {
-        internal Node head;
+        internal Node head; //new
         internal void Add(int data)
         {
             Node node = new Node(data);
             if (this.head == null)
-            {
                 this.head = node;
-            }
             else
             {
                 Node temp = head;
@@ -25,150 +23,168 @@ namespace LinkedList
                 }
                 temp.next = node;
             }
-            Console.Write("\n {0} inserted into linked list", node.data);
+            Console.WriteLine("{0} inserted into the linked list", node.data);
+
         }
-        public void AddinreverseOrder(int data)
+        internal void AddFirst(int data)
         {
-            Node newNode = new Node(data);
+            Node node = new Node(data);
             if (this.head == null)
+                this.head = node;
+            else
             {
-                this.head = newNode;
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into the Append linked list", node.data);
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("\nLinked list is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next; //temp=null
+            }
+            Console.WriteLine("\n");
+        }
+        internal Node InsertAtParticularPosition(int position, int data)
+        {
+            if (position < 1)
+                Console.WriteLine("Invalid position");
+            if (position == 1)
+            {
+                var newNode = new Node(data);
+                newNode.next = this.head;
+                head = newNode;
             }
             else
             {
                 Node temp = this.head;
-                head = newNode;
-                head.next = temp;
-            }
-            Console.Write("\n {0} inserted into linked list", newNode.data);
-        }
-        internal void Display()
-        {
-            Console.Write("\n Data of linked list : ");
-            Node temp = this.head;
-            if (temp == null)
-            {
-                Console.WriteLine("Linked list is empty");
-                return;
-            }
+                while (position-- != 0) //
+                {
 
-            while (temp != null)
-            {
-                Console.Write(temp.data + " ");
-                temp = temp.next;
+                    if (position == 1)
+                    {
+                        Node node = new Node(data);
+                        node.next = this.head.next;
+                        head.next = node;
+                        break;
+                    }
+                    temp = temp.next;//1000
+                }
+                if (position != 1)
+                    Console.WriteLine("Position out of range");
             }
-        }
-        internal Node InsertAtParticularPosition(int position, int data)
-        {
-            Node newestNode = new Node(data);
-            if (this.head == null)
-            {
-                return newestNode;
-            }
-            if (position == 0)
-            {
-                newestNode.next = this.head;
-                this.head = newestNode;
-                return this.head;
-            }
-            Node prev = null;
-            Node current = this.head;
-            int count = 0;
-            while (current != null && count < position)
-            {
-                prev = current;
-                current = current.next;
-                count++;
-            }
-            newestNode.next = prev.next;
-            prev.next = newestNode;
-            return this.head;
+            return head;
         }
         internal Node RemoveFirstNode()
         {
             if (this.head == null)
-            {
                 return null;
-            }
             this.head = this.head.next;
             return this.head;
         }
-        internal Node RemoveLastNode()
+        internal Node RemovaLastNode()
         {
             if (head == null)
-            {
                 return null;
-            }
             if (head.next == null)
-            {
                 return null;
-            }
-            Node NewNode = head;
-            while (NewNode.next.next != null)
+            Node newNode = head;
+            while (newNode.next.next != null)
             {
-                NewNode = NewNode.next;
+                newNode = newNode.next;
             }
-            NewNode.next = null;
+            newNode.next = null;
             return head;
         }
-        internal int Search(int value)
+        internal Node Search(int value)
         {
-            Node node = this.head;
-            int count = 0;
-            while (node != null)
+            while (this.head != null)
             {
-
-                if (node.data == value)
+                if (this.head.data == value)
                 {
-                    return count;
+                    return this.head;
                 }
-                node = node.next;
-                count++;
+                this.head = this.head.next;
             }
-            return count;
+            return null;
         }
-        internal void DeleteNodeAtParticularPosition(int position)
+        internal void RemoveNodeAtParticularPosition(int position)
         {
-            if (this.head == null)
-            {
-                Console.WriteLine("LinkedList is empty");
+
+            // If linked list is empty
+            if (head == null)
                 return;
-            }
-            Node temp = this.head;
+
+            // Store head node
+            Node temp = head;
+
+            // If head needs to be removed
             if (position == 0)
             {
-                this.head = temp.next;
+
+                // Change head
+                head = temp.next;
                 return;
             }
+
+            // Find previous node of the node to be deleted
             for (int i = 0; temp != null && i < position - 1; i++)
-            {
                 temp = temp.next;
 
-            }
+            // If position is more than number of nodes
             if (temp == null || temp.next == null)
-            {
                 return;
-            }
+
+            // Node temp->next is the node to be deleted
+            // Store pointer to the next of node to be deleted
             Node next = temp.next.next;
+
+            // Unlink the deleted node from list
             temp.next = next;
-            Size();
         }
-        internal void Size()
+        internal void sortList()
         {
-            Node temp = this.head;
-            int count = 0;
-            if (temp == null)
+            //Node current will point to head  
+            Node current = head, index = null;
+            int temp;
+
+            if (head == null)
             {
-                Console.WriteLine("LinkedList is empty");
                 return;
             }
-            while (temp != null)
+            else
             {
-                Console.Write(temp.data + " ");
-                temp = temp.next;
-                count++;
+                while (current != null)
+                {
+                    //Node index will point to node next to current  
+                    index = current.next;
+
+                    while (index != null)
+                    {
+                        //If current node's data is greater than index's node data, swap the data between them  
+                        if (current.data > index.data)
+                        {
+                            temp = current.data;
+                            current.data = index.data;
+                            index.data = temp;
+                        }
+                        index = index.next;
+                    }
+                    current = current.next;
+                }
             }
-            Console.WriteLine("\nLength of LinkedList is :-" + " " + count);
         }
     }
 }
